39183a0df8bedde84db7511e754fce4e19b30b87
aminebaha <aminebaha.info@gmail.com>
2020-09-16 13:04:27 +0200

    exo 1,2,3,4

--- exo.js
+++ exo.js
@@ -1,30 +1,65 @@
-var a = "Amine Amine";
+var a = "AaEAEAZEZA";
 var b = "amine amine";
 
 
 function lowerCase(a) {
-    return a.toLowerCase();
-    console.log(lower);
+    //match(/[A-Z]/, 'g'))
+    var chaine = "";
+    for(var i=0;i<a.length;i++) {
+        if(a.charAt(i).match(/[A-Z]/)) {
+             chaine += a.charAt(i).toLowerCase(); 
+        }
+        else {
+            chaine += a.charAt(i);
+        }
+    }
+    return chaine;
 
 }
 
-function upperCase(b) {
-return b.toUpperCase();
+function upperCase(a) {
+    var chaine = "";
+    for(var i=0;i<a.length;i++) {
+        if(a.charAt(i).match(/[a-z]/)) {
+             chaine += a.charAt(i).toUpperCase(); 
+        }
+        else {
+            chaine += a.charAt(i);
+        }
+    }
+    return chaine;
+
 }
 
 function length(a) {
-    return a.length;
-}
-
-function capitalize(s,restToLoweropt) {
-    
-    if(restToLoweropt==true) {
-        return s.toLowerCase(s.charAt(0));
+    var compteur=0;
+    for(var i=0;i<a.length;i++) {
+        compteur++;
     }
-    return s;
+    return compteur;
+}
+
+function capitalize(chaine,restToLoweropt) {
+    
+    if(restToLoweropt== true) {
+        var nvchaine = "";
+        nvchaine = chaine.charAt(0);
+        console.log("CAR : " + nvchaine);
+        for(var i=1;i<chaine.length;i++) {
+            nvchaine += chaine.charAt(i).toLowerCase();
+            console.log("LA NOUVELLE CHAINE : " +nvchaine);
+    }
+}
+    
+    if(restToLoweropt== null || restToLoweropt==false) {
+        console.log("aze");
+        return chaine;
+    }
+
+    
 
 }
 
-console.log(upperCase(a));
-console.log(length(a));
-console.log(capitalize(b,true));
+//console.log(length(b));
+//console.log(length(a));
+console.log(capitalize("One CODE", true));
